1b8c52e0927ac909e7973e3c22efbe068b6dd2a9
alpersarac <alper.sarac42@gmail.com>
2023-05-26 23:46:23 +0300

    styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ServerComponent } from './server/server.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  template: `
+  <app-server></app-server>
+  <app-server></app-server>`,
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent {
+
+}
